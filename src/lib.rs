3f0e39b18f31a761e90ba2a485448f1922e8432b
tjjfvi <t6@t6.fyi>
2024-01-16 16:45:51 -0500

    split into files

--- src/lib.rs
+++ src/lib.rs
@@ -10,30 +10,41 @@ pub use loanee::*;
 pub use may_leak::*;
 pub use place::*;
 
-#[test]
-fn test() {
+#[cfg(test)]
+mod test {
+  use super::*;
   use std::sync::atomic::{AtomicU32, Ordering};
-  let (r, b) = LoanedMut::new(Box::new(0));
-  *r = 1;
-  let mut x = Box::new(0);
-  b.place(&mut x);
-  *r = 2;
-  assert_eq!(*x, 2);
 
-  let (r, b) = loaned::Loaned::new(MayLeak::new(Box::new(AtomicU32::new(0))));
-  r.fetch_add(1, Ordering::Relaxed);
-  b.borrow().fetch_add(2, Ordering::Relaxed);
-  assert_eq!(b.load(Ordering::Relaxed), 3);
-  let mut x = Box::new(AtomicU32::new(0));
-  b.place(&mut x);
-  r.fetch_add(4, Ordering::Relaxed);
-  *x.as_mut().get_mut() += 5;
-  assert_eq!(*x.as_mut().get_mut(), 12);
+  #[test]
+  fn loaned_mut() {
+    let (r, b) = LoanedMut::new(Box::new(0));
+    *r = 1;
+    let mut x = Box::new(0);
+    b.place(&mut x);
+    *r = 2;
+    assert_eq!(*x, 2);
+  }
 
-  let (r, b) = LoanedMut::new(Box::new(123));
-  *r = 1;
-  let mut x = None;
-  b.place(&mut x);
-  *r = 2;
-  assert_eq!(x, Some(Box::new(2)));
+  #[test]
+  fn loaned_atomic() {
+    let (r, b) = Loaned::new(MayLeak::new(Box::new(AtomicU32::new(0))));
+    r.fetch_add(1, Ordering::Relaxed);
+    b.borrow().fetch_add(2, Ordering::Relaxed);
+    assert_eq!(b.load(Ordering::Relaxed), 3);
+    let mut x = Box::new(AtomicU32::new(0));
+    b.place(&mut x);
+    r.fetch_add(4, Ordering::Relaxed);
+    *x.as_mut().get_mut() += 5;
+    assert_eq!(*x.as_mut().get_mut(), 12);
+  }
+
+  #[test]
+  fn place_option() {
+    let (r, b) = LoanedMut::new(Box::new(123));
+    *r = 1;
+    let mut x = None;
+    b.place(&mut x);
+    *r = 2;
+    assert_eq!(x, Some(Box::new(2)));
+  }
 }
