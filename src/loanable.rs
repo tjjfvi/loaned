1f1e9d22b005b53900cd0ba548acb8b1ca47d64d
tjjfvi <t6@t6.fyi>
2024-01-18 16:54:58 -0500

    remove unused NEEDS_DROP constant

--- src/loanable.rs
+++ src/loanable.rs
@@ -14,41 +14,14 @@ use std::ops::Deref;
 ///
 /// This is closely related to whether the type can unconditionally implement
 /// `Unpin` (i.e. even when `Self::Target: !Unpin`).
-pub unsafe trait Loanable<'t>: Deref {
-  /// Whether or not this type should be dropped. When this is `true`, dropping
-  /// a `Loaned<Self>` will panic instead of leaking. This behavior can be opted
-  /// out of with `MayLeak`.
-  ///
-  /// Note that Rust never guarantees that `Drop` will be called, so even when
-  /// this is `true`, the value may still be leaked.
-  const NEEDS_DROP: bool;
-}
+pub unsafe trait Loanable<'t>: Deref {}
 
-unsafe impl<'t, T: ?Sized> Loanable<'t> for Box<T> {
-  const NEEDS_DROP: bool = true;
-}
-
-unsafe impl<'t, T> Loanable<'t> for Vec<T> {
-  const NEEDS_DROP: bool = true;
-}
-
-unsafe impl<'t> Loanable<'t> for String {
-  const NEEDS_DROP: bool = true;
-}
-
-unsafe impl<'t, T: ?Sized> Loanable<'t> for std::rc::Rc<T> {
-  const NEEDS_DROP: bool = true;
-}
-
-unsafe impl<'t, T: ?Sized> Loanable<'t> for std::sync::Arc<T> {
-  const NEEDS_DROP: bool = true;
-}
+unsafe impl<'t, T: ?Sized> Loanable<'t> for Box<T> {}
+unsafe impl<'t, T> Loanable<'t> for Vec<T> {}
+unsafe impl<'t> Loanable<'t> for String {}
+unsafe impl<'t, T: ?Sized> Loanable<'t> for std::rc::Rc<T> {}
+unsafe impl<'t, T: ?Sized> Loanable<'t> for std::sync::Arc<T> {}
 
 // The usefulness of this implementation is dubious at best, but it's here for completeness.
-unsafe impl<'t, 'a: 't, T: ?Sized> Loanable<'t> for &'a T {
-  const NEEDS_DROP: bool = false;
-}
-
-unsafe impl<'t, 'a: 't, T: ?Sized> Loanable<'t> for &'a mut T {
-  const NEEDS_DROP: bool = false;
-}
+unsafe impl<'t, 'a: 't, T: ?Sized> Loanable<'t> for &'a T {}
+unsafe impl<'t, 'a: 't, T: ?Sized> Loanable<'t> for &'a mut T {}
